e47a7b39974f7e6ec035595fc3913965c87b9bfb
Raman <40736787@live,napier.ac.uk>
2025-12-10 16:54:00 +0000

    Added review page

--- cinelog/models.py
+++ cinelog/models.py
@@ -10,15 +10,32 @@ class User(UserMixin, db.Model):
     password_hash = db.Column(db.String(200), nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
 
+    # relationship: one user can have many reviews
+    reviews = db.relationship("Review", backref="user", lazy=True)
+
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
 
+
 class Movie(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(200), nullable=False)
+    title = db.Column(db.String(200))
     year = db.Column(db.String(10))
     status = db.Column(db.String(50), default="watchlist")
+    poster_path = db.Column(db.String(300))
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+
+    # relationship: one movie can have many reviews
+    reviews = db.relationship("Review", backref="movie", lazy=True)
+
+
+class Review(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.Text, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    movie_id = db.Column(db.Integer, db.ForeignKey("movie.id"))
